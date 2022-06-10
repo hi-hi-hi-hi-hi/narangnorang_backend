fd8b48db9cdf88a77c7eba8ee19c7d83fe686d1e
kwondongwook <kwondongwook1@gmail.com>
2022-06-10 23:26:46 +0900

    refactor : SPA로 리팩토링 시작

--- src/main/resources/static/js/ask/ask.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$("#answerContentPop").on("click", function(){
-		if($("#answerContentArea").text()){
-			$("#answerContentArea").text("");
-		}else{
-			$("#answerContentArea").text("A. 노랑이는 떡볶이를 좋아해요");
-		}
-	})
-})
