04c0e97cf4361c287e975db3d5cf3fa995c9f01b
kwondongwook <kwondongwook1@gmail.com>
2022-06-15 16:48:08 +0900

    fix : TestController를 Controller에서 RestController로 수정

--- src/main/java/com/narangnorang/controller/TestController.java
+++ src/main/java/com/narangnorang/controller/TestController.java
@@ -1,61 +1,52 @@
 package com.narangnorang.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.narangnorang.dto.MemberDTO;
 import com.narangnorang.dto.TestResultDTO;
 import com.narangnorang.service.TestService;
 
-@Controller
+@RestController
 public class TestController {
 
 	@Autowired
 	TestService testService;
 
-	// @GetMapping({ "테스트 목록 보기", "테스트 보기", "테스트 결과 보기" })
-	@GetMapping({ "/test", "/test/{type}", "/test/{type}/{result}" })
-	public String test(@PathVariable(value = "type", required = false) @ModelAttribute("type") Integer type,
-			@PathVariable(value = "result", required = false) @ModelAttribute("result") Integer result)
-			throws Exception {
-		return "test";
-	}
-
 	// 테스트 결과 저장
-	@PostMapping("/test/{type}/{result}")
-	public String insert(HttpSession session, @PathVariable("type") int type, @PathVariable("result") int result)
-			throws Exception {
+	@PostMapping("/api/test/{type}/{result}")
+	public Map<String, Object> insert(HttpSession session, @PathVariable("type") int type,
+			@PathVariable("result") int result) throws Exception {
+		Map<String, Object> response = new HashMap<String, Object>();
+		response.put("flag", false);
 		MemberDTO login = (MemberDTO) session.getAttribute("login");
 		int memberId = login.getId();
 		TestResultDTO testResultDTO = new TestResultDTO(0, memberId, null, type, result);
-		testService.insert(testResultDTO);
-		return "redirect:/test/" + type + "/" + result;
+		int cnt = testService.insert(testResultDTO);
+		if (cnt == 1) {
+			response.put("flag", true);
+		}
+		return response;
 	}
 
 	// 지난 테스트 결과
-	@GetMapping("/test/results")
-	public String selectList(HttpSession session, Model model) throws Exception {
-		MemberDTO memberDTO = (MemberDTO) session.getAttribute("login");
-		int memberId = memberDTO.getId();
+	@GetMapping("/api/test/results")
+	public List<TestResultDTO> selectList(HttpSession session, Model model) throws Exception {
+		MemberDTO login = (MemberDTO) session.getAttribute("login");
+		int memberId = login.getId();
 		List<TestResultDTO> testResultList = testService.selectList(memberId);
 		model.addAttribute("testResultList", testResultList);
-		return "test";
-	}
-
-	// 에러 처리
-	@ExceptionHandler({ Exception.class })
-	public String error() throws Exception {
-		return "common/error";
+		return testResultList;
 	}
 
 }
