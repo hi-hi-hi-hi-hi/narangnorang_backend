a07b154a87fef911576ebaec5cc96b51b3ed8909
kwondongwook <kwondongwook1@gmail.com>
2022-06-25 21:13:37 +0900

    rename : Norang to ChatBot

--- src/main/java/com/narangnorang/service/ChatBotServiceImpl.java
+++ src/main/java/com/narangnorang/service/ChatBotServiceImpl.java
@@ -4,53 +4,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.narangnorang.dao.NorangDAO;
 import com.narangnorang.dto.ChallengeDTO;
+import com.narangnorang.dao.ChatBotDAO;
 import com.narangnorang.dto.DailyLogDTO;
 import com.narangnorang.dto.MoodStateDTO;
 
-@Service("norangService")
-public class NorangServiceImpl implements NorangService {
+@Service("chatBotService")
+public class ChatBotServiceImpl implements ChatBotService {
 
 	@Autowired
-	NorangDAO norangDAO;
+	ChatBotDAO chatBotDAO;
 
 	// 챌린지 조회(하루)
 	@Override
 	public ChallengeDTO selectChallenge(ChallengeDTO challengeDTO) throws Exception {
-		return norangDAO.selectChallenge(challengeDTO);
+		return chatBotDAO.selectChallenge(challengeDTO);
 	}
 
 	// 일일 데이터 조회(하루)
 	@Override
 	public DailyLogDTO selectDailyLog(DailyLogDTO dailyLogDTO) throws Exception {
-		return norangDAO.selectDailyLog(dailyLogDTO);
+		return chatBotDAO.selectDailyLog(dailyLogDTO);
 	}
 
 	// 기분 상태 조회(접속일시 이후)
 	@Override
 	public MoodStateDTO selectMoodState(MoodStateDTO moodStateDTO) throws Exception {
-		return norangDAO.selectMoodState(moodStateDTO);
+		return chatBotDAO.selectMoodState(moodStateDTO);
 	}
 
 	// 챌린지 저장
 	@Transactional
 	@Override
 	public int insertChallenge(ChallengeDTO challengeDTO) throws Exception {
-		norangDAO.updatePoint(challengeDTO); // 챌린지 보상
-		return norangDAO.insertChallenge(challengeDTO);
+		chatBotDAO.updatePoint(challengeDTO); // 챌린지 보상
+		return chatBotDAO.insertChallenge(challengeDTO);
 	}
 
 	// 일일 데이터 저장
 	@Override
 	public int insertDailyLog(DailyLogDTO dailyLogDTO) throws Exception {
-		return norangDAO.insertDailyLog(dailyLogDTO);
+		return chatBotDAO.insertDailyLog(dailyLogDTO);
 	}
 
 	// 기분 상태 저장
 	@Override
 	public int insertMoodState(MoodStateDTO moodStateDTO) throws Exception {
-		return norangDAO.insertMoodState(moodStateDTO);
+		return chatBotDAO.insertMoodState(moodStateDTO);
 	}
 
 }
