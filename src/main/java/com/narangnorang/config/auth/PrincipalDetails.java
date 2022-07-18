4c226fab603ec4d7b4dbc68d667c6f315c41ae0c
이전영 <ijeon-yeong@ijeon-yeong-ui-MacBookAir.local>
2022-07-18 15:00:39 +0900

    commit

---
+++ src/main/java/com/narangnorang/config/auth/PrincipalDetails.java
@@ -0,0 +1,63 @@
+package com.narangnorang.config.auth;
+
+import com.narangnorang.dto.MemberDTO;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PrincipalDetails implements UserDetails {
+
+    private MemberDTO memberDTO;
+
+    public PrincipalDetails(MemberDTO memberDTO) {
+        this.memberDTO = memberDTO;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        collect.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                // return memberDTO.getPrivilege();
+                return null;
+            }
+        });
+        return collect;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
