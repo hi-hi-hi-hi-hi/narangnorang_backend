b9807bac5b19b48fa40c5f67e02a3a7695a0d141
이전영 <ijeon-yeong@ijeon-yeong-ui-MacBookAir.local>
2022-07-28 00:43:52 +0900

    Update PrincipalOauth2UserService.java

--- src/main/java/com/narangnorang/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/narangnorang/config/oauth/PrincipalOauth2UserService.java
@@ -2,6 +2,8 @@ package com.narangnorang.config.oauth;
 
 import com.narangnorang.dto.MemberDTO;
 import com.narangnorang.service.MemberService;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -9,6 +11,12 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import com.narangnorang.config.auth.PrincipalDetails;
+import com.narangnorang.dto.MemberDTO;
+import com.narangnorang.service.MemberService;
+
+import java.util.HashMap;
 
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
@@ -21,24 +29,69 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        String provider = userRequest.getClientRegistration().getClientId();
-        String providerId = oAuth2User.getAttribute("sub");
-        String name = provider + "_" + providerId;
-        String password = bCryptPasswordEncoder.encode(providerId);
-        String role = "ROLE_USER";
-        String email = oAuth2User.getAttribute("email");
-
-        try {
-            MemberDTO member = memberService.selectByKakaoId(providerId);
-            if(member == null) {
-
-            }else {
-
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+        if (provider.equals("google")) {
+            String providerId = oAuth2User.getAttribute("sub");
+            String name = provider + "_" + providerId;
+            String password = bCryptPasswordEncoder.encode(providerId);
+            String email = oAuth2User.getAttribute("email");
+            try {
+                MemberDTO member = memberService.selectByKakaoId(providerId);
+                if (member == null) {
+                    MemberDTO mDTO = new MemberDTO();
+                    // 회원가입 처리
+                    int randomNumber = 0;
+                    String tmpId = "";
+                    randomNumber = (int) Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 99999999;
+                    tmpId = "google" + randomNumber + "@g.com";
+                    mDTO.setEmail(tmpId);
+                    mDTO.setPassword(password);
+                    mDTO.setName(name);
+                    mDTO.setPhone("미등록");
+                    mDTO.setRegion("미등록");
+                    mDTO.setKakaoId(providerId);
+                    memberService.generalSignUp(mDTO);
+                    MemberDTO memberDTO = memberService.selectByKakaoId(providerId);
+                    return new PrincipalDetails(memberDTO, oAuth2User.getAttributes());
+                } else {
+                    return new PrincipalDetails(member, oAuth2User.getAttributes());
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } else {
+            String xxx = oAuth2User.getName();
+            HashMap<String, Object> retMap = new Gson().fromJson(
+                    xxx, new TypeToken<HashMap<String, Object>>() {}.getType()
+            );
+            String providerId = (String) retMap.get("id");
+            String name = (String) retMap.get("name");
+            String password = bCryptPasswordEncoder.encode(providerId);
+            String email = (String) retMap.get("email");
+            try {
+                MemberDTO member = memberService.selectByKakaoId(providerId);
+                if (member == null) {
+                    MemberDTO mDTO = new MemberDTO();
+                    // 회원가입 처리
+                    int randomNumber = 0;
+                    String tmpId = "";
+                    randomNumber = (int) Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 99999999;
+                    tmpId = "naver" + randomNumber + "@n.com";
+                    mDTO.setEmail(tmpId);
+                    mDTO.setPassword(password);
+                    mDTO.setName(name);
+                    mDTO.setPhone("미등록");
+                    mDTO.setRegion("미등록");
+                    mDTO.setKakaoId(providerId);
+                    memberService.generalSignUp(mDTO);
+                    MemberDTO memberDTO = memberService.selectByKakaoId(providerId);
+                    return new PrincipalDetails(memberDTO, oAuth2User.getAttributes());
+                } else {
+                    return new PrincipalDetails(member, oAuth2User.getAttributes());
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
-
-        return super.loadUser(userRequest);
     }
 }
