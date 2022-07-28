4dbb50a1f5149fd2121877f74fc4f22ae4d30a50
dawonseo <oloielo@naver.com>
2022-07-28 12:16:18 +0900

    chore: MemberService implements 관련 에러 수정

--- src/main/java/com/narangnorang/service/MemberServiceImpl.java
+++ src/main/java/com/narangnorang/service/MemberServiceImpl.java
@@ -1,13 +1,5 @@
 package com.narangnorang.service;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.narangnorang.dao.MemberDAO;
 import com.narangnorang.dao.MiniroomDAO;
 import com.narangnorang.dto.MemberDTO;
